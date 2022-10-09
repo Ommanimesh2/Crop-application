5221b0f2d3fbdde42dd19858dedff097f8c8e212
Ommanimesh2 <93693087+Ommanimesh2@users.noreply.github.com>
2022-10-09 10:34:10 +0530

    Another commit

--- src/App.js
+++ src/App.js
@@ -18,22 +18,35 @@ function App(){
   const style2={
     display:'block'
   }
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 2500);
-  }, []);
+  // useEffect(() => {
+  // 
+  // }, []);
   
- setTimeout(()=>{ 
+useEffect(() => {
+  setLoading(true);
+
+
   var map1 = L.DomUtil.get('map'); if(map1 != null){ map1._leaflet_id = null; }
- 
+  
   var map = L.map('map').setView([29.8665,77.9060], 14);
+  
+
   var googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
     maxZoom: 20,
     subdomains:['mt0','mt1','mt2','mt3']
   }).addTo(map);
-  
+  var latlngs = [  
+  [29.83900608249045,77.90231862375538],
+  [29.83900608249045,77.95107045481006],
+  [29.88188113949949,77.95107045481006],
+  [29.88188113949949,77.90231862375538],
+  [29.83900608249045,77.90231862375538],
+
+];
+  var rectOptions = {color: 'Red', weight: 0}
+  var polyline = L.polyline(latlngs, {color: 'red'});
+ 
+  polyline.addTo(map);
   // osm.addTo(map);
   var datas = L.geoJSON(data,{
     onEachFeature:onEachFeature
@@ -41,12 +54,15 @@ function App(){
   function onEachFeature(features, layer) {
     // does this feature have a property named popupContent?
     layer.on('click',(e)=>{
-     setParam(e.target.feature.properties.id)
-     setLeftDet(e.target.feature.properties)
+      setParam(e.target.feature.properties.id)
+      setLeftDet(e.target.feature.properties)
       
     })
   }
-},2000)
+  setLoading(false);
+
+},[])
+  
   return (
     <>
      {loading ? (<Loading/>) :
